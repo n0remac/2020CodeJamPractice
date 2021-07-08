54ab8c6bd6d0975110864d52f380f8fc530acd14
Antsthebul <anthony.allen.srt@gmail.com>
2021-07-07 20:48:28 -0400

    Updated app.py to display split rows. Added antsthebul panel

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from rich.live import Live
 from rich.text import Text
 from rich.panel import Panel
 
+from panels.cameron import cameron_panel
+from panels.antsthebul import ants_panel
 
 def run_app(layout):
     pass
 
 
-layout = Layout()
+def setup(layout):
+    layout.split_column(
+        Layout(name="top"),
+        Layout(name="bottom"),
+    )
+    layout["top"].split_row(
+        Layout(cameron_panel()),
+        Layout(ants_panel()))
+
+main_layout = Layout()
 
 if __name__ == "__main__":
     with Live(layout, refresh_per_second=10, screen=True):
