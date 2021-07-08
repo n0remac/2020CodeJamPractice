cf7f9a3745ae2201dd66d1de5268899c3a169c9b
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-08 01:46:06 -0400

    Added Frontogenesis' panel to app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from rich.live import Live
 
 from panels.cameron import cameron_panel
 from panels.antsthebul import ants_panel
+from panels.frontogenesis import frontogenesis_panel
 
 
 def update_layout(layout):
@@ -16,7 +17,8 @@ def setup(layout):
     )
     layout["top"].split_row(
         Layout(cameron_panel()),
-        Layout(ants_panel()))
+        Layout(ants_panel()),
+        Layout(frontogenesis_panel()))
 
 main_layout = Layout()
 
--- panels/frontogenesis.py
+++ panels/frontogenesis.py
@@ -6,6 +6,3 @@ from rich import print as rprint
 def frontogenesis_panel():
     text_to_add = Text(text='Frontogenesis has appeared!'.center(3),style='bold magenta')
     return Panel(text_to_add, title='A wild..',width=20,height=4)
-
-
-rprint(frontogenesis_panel())
