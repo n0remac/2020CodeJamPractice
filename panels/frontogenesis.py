2ba5d4ae6dce3daf662e941e37b3b8cd9c7e1682
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-08 02:13:43 -0400

    Removed unused import from frontogenesis.py

--- panels/frontogenesis.py
+++ panels/frontogenesis.py
@@ -1,7 +1,6 @@
 from rich.panel import Panel
 from rich.text import Text
 
-
 def frontogenesis_panel():
     text_to_add = Text(text='Frontogenesis has appeared!'.center(3),style='bold magenta')
     return Panel(text_to_add, title='A wild..',width=20,height=4)
