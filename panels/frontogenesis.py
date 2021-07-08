c91570fae73e348d88a777cdb873c41116217291
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-07-08 01:32:55 -0400

    Added frontogenesis panel file

---
+++ panels/frontogenesis.py
@@ -0,0 +1,11 @@
+from rich.panel import Panel
+from rich.text import Text
+from rich import print as rprint
+
+
+def frontogenesis_panel():
+    text_to_add = Text(text='Frontogenesis has appeared!'.center(3),style='bold magenta')
+    return Panel(text_to_add, title='A wild..',width=20,height=4)
+
+
+rprint(frontogenesis_panel())
